25a352f56460ca97599e38bdeb83d06b3b1535f4
erik-petrov <erikpetrov23@gmail.com>
2024-12-01 19:57:58 +0200

    add shaders

--- modid_resources/scrape.js
+++ modid_resources/scrape.js
@@ -1,25 +1,26 @@
 //SCRIPT TEGI: ERIK PETROV
-var mods;
-fetch("https://api.modrinth.com/v2/projects_random?count=10")
-.then((response) => response.json())
+fetch("https://api.modrinth.com/v2/projects_random?count=10") // Teeb päringu Modrinthi API-le, et saada 10 juhuslikku projekti
+.then((response) => response.json()) // Konverteerib vastuse JSON-iks
 .then((json) => {
-    var t = document.getElementById("needsChange");
-    //for some reason the api returns a random amount of mods as well..
-    t.innerHTML = t.innerHTML.replace("**COUNT**", json.length);
-    (function(){
-        var ul = document.getElementById("mod_list").getElementsByTagName("ul")[0];
+    var t = document.getElementById("needsChange"); // Leiab elemendi id-ga 'needsChange'
     
-        json.forEach(renderProductList);
+    // API tagastab mõnikord ka juhusliku arvu mode...
+    t.innerHTML = t.innerHTML.replace("**COUNT**", json.length); // Asendab '**COUNT**' tegeliku modide arvuga
+
+    (function(){
+        var ul = document.getElementById("mod_list").getElementsByTagName("ul")[0]; // Leiab esimese <ul> elemendi elemendis id-ga 'mod_list'
+    
+        json.forEach(renderProductList); // Läbib iga mod-i JSON andmetes ja kutsub funktsiooni renderProductList
     
         function renderProductList(element) {
-            var li = document.createElement('li');
-            li.setAttribute('class','item');
-            ul.appendChild(li);
-    
+            var li = document.createElement('li'); // Loob uue <li> elemendi
+            li.setAttribute('class','item'); // Määrab <li> klassiks 'item'
+            ul.appendChild(li); // Lisab <li> elemendi <ul> sisse
+
+            // Loob vormindatud stringi, mis sisaldab linki mod-i lehele
             let formattedString = `<span><a href='https://modrinth.com/mod/${element["slug"]}' target='_blank'>${element["title"]}</a></span>`;
 
-            li.innerHTML=li.innerHTML + formattedString;
+            li.innerHTML = li.innerHTML + formattedString; // Lisab vormindatud stringi <li> sisu juurde
         }
-    })();
+    })(); // Kutsutakse kohe välja anonüümne funktsioon modide loendi renderdamiseks
 });
-
